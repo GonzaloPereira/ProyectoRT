b764612dc82c9ee4e8b23905604a66c1c7e77e6d
Gonzalo Pereira <gonzalo.pereira.r@uni.pe>
2021-12-13 20:24:31 -0500

    Fix turbulence bug

--- src/Control/Control.js
+++ src/Control/Control.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import SendData from "./SendData";
 import ReceiveData from "./ReceiveData";
 import "./Control.css";
+import { ref, onValue } from "firebase/database";
+import { database } from "./firebase";
 
 export default function Control() {
+  const array_zeros = [];
+  for (let i = 0; i < 100; i++) array_zeros.push(0);
+
+  const [x1, set_x1] = useState(array_zeros);
+  const [dx1, set_dx1] = useState(array_zeros);
+  const [v1, set_v1] = useState(array_zeros);
+
+  const [x2, set_x2] = useState(array_zeros);
+  const [dx2, set_dx2] = useState(array_zeros);
+  const [v2, set_v2] = useState(array_zeros);
+
+  const charts_ref = ref(database, "charts");
+  useEffect(() => {
+    onValue(charts_ref, (snapshot) => {
+      const value = snapshot.val();
+      set_x1((prev) => prev.slice(1));
+      set_dx1((prev) => prev.slice(1));
+      set_v1((prev) => prev.slice(1));
+      set_x2((prev) => prev.slice(1));
+      set_dx2((prev) => prev.slice(1));
+      set_v2((prev) => prev.slice(1));
+      set_x1((prev) => [...prev, value.x1]);
+      set_dx1((prev) => [...prev, value.dx1]);
+      set_v1((prev) => [...prev, value.v1]);
+      set_x2((prev) => [...prev, value.x2]);
+      set_dx2((prev) => [...prev, value.dx2]);
+      set_v2((prev) => [...prev, value.v2]);
+    });
+  }, []);
+
   return (
     <div className="control">
       <SendData />
-      <ReceiveData />
+      <ReceiveData x1={x1} dx1={dx1} v1={v1} x2={x2} dx2={dx2} v2={v2} />
     </div>
   );
 }
--- src/Control/ReceiveData.js
+++ src/Control/ReceiveData.js
@@ -1,81 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-import { ref, onValue } from "firebase/database";
-import { database } from "./firebase";
 
-export default function ReceiveData() {
-  let array_zeros = [];
-  for (let i = 0; i < 100; i++) array_zeros.push(0);
-
-  const [x1, set_x1] = useState(array_zeros);
-  const [dx1, set_dx1] = useState(array_zeros);
-  const [v1, set_v1] = useState(array_zeros);
-
-  const [x2, set_x2] = useState(array_zeros);
-  const [dx2, set_dx2] = useState(array_zeros);
-  const [v2, set_v2] = useState(array_zeros);
-
-  const x1_ref = ref(database, "charts/x1");
-  const dx1_ref = ref(database, "charts/dx1");
-  const v1_ref = ref(database, "charts/v1");
-
-  const x2_ref = ref(database, "charts/x2");
-  const dx2_ref = ref(database, "charts/dx2");
-  const v2_ref = ref(database, "charts/v2");
-
-  useEffect(() => {
-    onValue(x1_ref, (snapshot) => {
-      set_x1((prev) => {
-        let temp = prev.slice(1);
-        return [...temp, snapshot.val()];
-      });
-      set_dx1((prev) => {
-        let temp = prev.slice(1);
-        return [...temp, temp[temp.length - 1]];
-      });
-    });
-
-    onValue(dx1_ref, (snapshot) => {
-      set_dx1((prev) => {
-        let temp = prev.slice(1);
-        return [...temp, snapshot.val()];
-      });
-    });
-
-    onValue(v1_ref, (snapshot) => {
-      set_v1((prev) => {
-        let temp = prev.slice(1);
-        return [...temp, snapshot.val()];
-      });
-    });
-
-    onValue(x2_ref, (snapshot) => {
-      set_x2((prev) => {
-        let temp = prev.slice(1);
-        return [...temp, snapshot.val()];
-      });
-      set_dx2((prev) => {
-        let temp = prev.slice(1);
-        return [...temp, temp[temp.length - 1]];
-      });
-    });
-
-    onValue(dx2_ref, (snapshot) => {
-      set_dx2((prev) => {
-        let temp = prev.slice(1);
-        return [...temp, snapshot.val()];
-      });
-    });
-
-    onValue(v2_ref, (snapshot) => {
-      set_v2((prev) => {
-        let temp = prev.slice(1);
-        return [...temp, snapshot.val()];
-      });
-    });
-  }, []);
-
+export default function ReceiveData({ x1, dx1, v1, x2, dx2, v2 }) {
   const optionsGeneral = {
     chart: {
       type: "line",
